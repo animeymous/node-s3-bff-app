0896ee5149a64a38a4779745e6dbb07813a2f22b
Vikash Mishra <workpurpose@gmail.com>
2024-03-16 13:08:12 +0530

    connected local db

--- src/infra/mongoose/mongoose-config.service.ts
+++ src/infra/mongoose/mongoose-config.service.ts
@@ -8,13 +8,17 @@ export class MongooseConfigService implements MongooseOptionsFactory{
     constructor(private configService: ConfigService) {}
 
     createMongooseOptions(): MongooseModuleOptions | Promise<MongooseModuleOptions> {
-        const username = this.configService.get("DATABASE_USER");
-        const password = this.configService.get("DATABASE_PASSWORD");
-        const host = this.configService.get("DATABASE_HOST");
-        const db = this.configService.get("DATABASE_NAME")
+        // const username = this.configService.get("DATABASE_USER");
+        // const password = this.configService.get("DATABASE_PASSWORD");
+        // const host = this.configService.get("DATABASE_HOST");
+        // const db = this.configService.get("DATABASE_NAME")
 
-        const uri = `mongodb+srv://${username}:${password}@${host}/${db}?retryWrites=true&w=majority&appName=Cluster0`;
+        // const uri = `mongodb+srv://${username}:${password}@${host}/${db}?retryWrites=true&w=majority&appName=Cluster0`;
 
+        // return {
+        //     uri
+        // }
+        let uri = "mongodb://localhost:27017/node-s3";
         return {
             uri
         }
